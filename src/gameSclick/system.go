0b1205c244f3f88bc04fc91dd8abc8e0f8192e78
DerWaldFried <verwaltung@fabelgrund.de>
2026-02-27 22:21:01 +0100

    Refactor user creation logic to prevent race conditions and improve error handling

--- src/gameSclick/system.go
+++ src/gameSclick/system.go
@@ -29,17 +29,31 @@ func getDistroID() string {
 }
 
 func (cfg *Config) addUser() error {
+	// Check again the System for the user, because if we do it before, we can have a race condition where the user is created after the
+	// check but before the creation, which would lead to an error. By checking again right before the creation, we can avoid this issue.
+	_, err := exec.LookPath("id")
+	if err == nil {
+		checkUser := exec.Command("id", "-u", cfg.Username)
+		if err := checkUser.Run(); err == nil {
+			return fmt.Errorf("user '%s' already exists, skipping creation", cfg.Username)
+		}
+	}
+
 	pterm.Info.Printfln("Creating system user '%s'...", cfg.Username)
 
-	// --disabled-password stops the system from asking for a password during user creation, we will set it later
-	// --gecos "" passes empty information for the user (like full name, room number, etc.) to avoid interactive prompts
+	// Adding User with no password and no shell access (disabled login)
 	cmd := exec.Command("sudo", "adduser", "--disabled-password", "--gecos", "", cfg.Username)
-
-	err := cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("could not create user: %v", err)
 	}
 
-	pterm.Success.Printfln("User '%s' created successfully.", cfg.Username)
+	// Set Passwort (Send with Pipe on chpasswd)
+	passCmd := exec.Command("sudo", "chpasswd")
+	passCmd.Stdin = strings.NewReader(fmt.Sprintf("%s:%s", cfg.Username, cfg.Userpasswd))
+	if err := passCmd.Run(); err != nil {
+		return fmt.Errorf("could not set password for user: %v", err)
+	}
+
+	pterm.Success.Printfln("User '%s' created and password set.", cfg.Username)
 	return nil
 }
